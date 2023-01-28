f01bfd92b5621332013399d664738091ee1e10b5
soomin <qkrtnals970804@gmail.com>
2023-01-28 23:43:04 +0900

    JSDoc

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,20 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config) {
   return true;
 }
 
+/**
+ * Exits the project
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }
